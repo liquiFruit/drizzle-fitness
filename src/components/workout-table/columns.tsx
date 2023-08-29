011a0b71fb7d9fd4c423652c5b66b4462111a9b6
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-30 00:17:13 +0200

    feat: add action menu to table

--- src/components/workout-table/columns.tsx
+++ src/components/workout-table/columns.tsx
@@ -1,9 +1,20 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
 
 import { Workout } from "@/db"
 import { fuzyTime } from "@/lib/utils"
+
+import { Button } from "@/components/ui/button"
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export const columns: ColumnDef<Workout>[] = [
 	{
@@ -16,6 +27,29 @@ export const columns: ColumnDef<Workout>[] = [
 		cell: ({ row }) => {
 			const date = fuzyTime(row.getValue("date"))
 			return <div className="text-right font-medium">{date}</div>
+		},
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const workout = row.original
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+
+					<DropdownMenuContent align="start">
+						<DropdownMenuLabel>Quick Actions</DropdownMenuLabel>
+						<DropdownMenuItem>
+							View workout details
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
 		},
 	},
 ]
