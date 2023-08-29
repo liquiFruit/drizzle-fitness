c5693c26082467159e1b4b0edc1ca4b1fb0fee2e
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-29 14:52:35 +0200

    add user image in nav

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -3,21 +3,29 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
+import Image from "next/image"
 export function AuthButton() {
-	const { status } = useSession()
+	const { status, data } = useSession()
+	const user = data?.user!
 
 	return (
 		<>
 			{status == "authenticated" ? (
-				<Button variant={"secondary"} onClick={() => signOut()}>
-					Sign out
-				</Button>
+				<div
+					onClick={() => signOut()}
+					className="cursor-pointer relative aspect-square rounded-full overflow-hidden w-8"
+				>
+					<Image
+						src={user.image!}
+						alt={user.name!}
+						fill
+						className="object-cover"
+					/>
+				</div>
 			) : status == "unauthenticated" ? (
 				<Button onClick={() => signIn("github")}>Sign in</Button>
 			) : (
-				<Button variant={"secondary"} disabled>
-					<Loader2 className="animate-spin" />
-				</Button>
+				<Loader2 className="animate-spin" size={32} />
 			)}
 		</>
 	)
