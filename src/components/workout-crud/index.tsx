eae203959c21943dedf1c1bf571604c1d010cb2a
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-09-01 10:43:02 +0200

    fix: implement new state

--- src/components/workout-crud/index.tsx
+++ src/components/workout-crud/index.tsx
@@ -1,16 +1,32 @@
 "use client"
 
-import { useState } from "react"
+import { memo, useState } from "react"
 
 import { DatePicker } from "@/components/ui/date-picker"
 import { ExerciseSetCrud } from "./set-crud"
-import { ExerciseSet } from "@/db"
+import { ExerciseSet, Workout } from "@/db"
 
-export function WorkoutCrud() {
-	const [sets, setSets] = useState<ExerciseSet[]>([])
+interface FullWorkout {
+	date: Date
+	sets: ExerciseSet[]
+}
 
-	const addSet = (set: ExerciseSet) => {
-		setSets([...sets, set])
+type WorkoutCrudProps = {
+	initialState?: Partial<FullWorkout>
+}
+export function WorkoutCrud({ initialState }: WorkoutCrudProps) {
+	// todo: check preformance by printing "rendering" for every set in sets
+	const [workout, setWorkout] = useState<FullWorkout>({
+		date: new Date(),
+		sets: [],
+		...initialState,
+	})
+
+	const addSet = (set: ExerciseSet) =>
+		setWorkout({ ...workout, sets: [...workout.sets, set] })
+
+	const setDate = (date: Date | undefined) => {
+		if (date) setWorkout({ ...workout, date })
 	}
 
 	return (
@@ -22,27 +38,30 @@ export function WorkoutCrud() {
 			<h3>Workout Date</h3>
 			<DatePicker
 				className="w-full"
-				date={new Date()}
-				setDate={(date) => alert(date)}
+				date={workout.date}
+				setDate={setDate}
 			/>
 
 			{/* Exercise */}
 			<h3>Add Exercise Set</h3>
-			<ExerciseSetCrud
-				onCreate={addSet}
-				initialState={{
-					details: 0,
-				}}
-			/>
+			<ExerciseSetCrud onCreate={addSet} initialState={{}} />
 
 			<h3>Sets</h3>
 			<div>
-				{sets.map((set, i) => (
-					<p key={i}>
-						{set.exerciseId} - {set.details}
-					</p>
+				{workout.sets.map((set, i) => (
+					<Set key={i} set={set} />
 				))}
 			</div>
 		</section>
 	)
 }
+
+const Set = memo(({ set }: { set: ExerciseSet }) => {
+	console.log("rendering " + set.details)
+
+	return (
+		<p>
+			{set.exerciseId} - {set.details}
+		</p>
+	)
+})
