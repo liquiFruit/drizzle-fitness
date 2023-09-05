3fef1761bc32c412bb47066d37ca20791422d135
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-09-05 21:58:17 +0200

    feat: split schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from "drizzle-kit"
-import dotevn from "dotenv"
-dotevn.config()
+import "dotenv/config"
 
 export default {
-  schema: "./src/db/schema.ts",
+  schema: "./src/lib/db/schema/**/*",
   out: "./src/db/migrations",
   driver: "better-sqlite",
   dbCredentials: {
--- src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3"
-import Database from "better-sqlite3"
-
-import drizzleConfig from "@/../drizzle.config"
-import * as schema from "@/db/schema"
-
-export const db = drizzle(new Database(drizzleConfig.dbCredentials.url), { schema })
-export * from "./schema"
